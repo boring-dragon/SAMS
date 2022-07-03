1097196650d9b266bfabdb2c2c702d44c36e8273
Mohamed jinas <j@live.mv>
2022-07-04 00:24:24 +0500

    WIP

---
+++ resources/js/Pages/Admin/Modules/Index.vue
@@ -0,0 +1,20 @@
+<script setup>
+import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
+import { Head } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+    <Head title="Modules" />
+
+    <BreezeAuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Modules
+            </h2>
+        </template>
+
+        <div class="py-12">
+
+        </div>
+    </BreezeAuthenticatedLayout>
+</template>
