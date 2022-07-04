d9cd9396c34561999ba39b615027941572c171c3
Mohamed jinas <j@live.mv>
2022-07-04 21:34:25 +0500

    WIP

--- resources/js/Shared/Search.vue
+++ resources/js/Shared/Search.vue
@@ -11,7 +11,7 @@ const props = defineProps({
 <template>
 	<div class="flex-1 pr-4">
 		<div class="relative md:w-1/3">
-			<input :value="props.modelValue" @input="$emit('update:modelValue', $event.target.value)" autocomplete="off" class="text-sm w-full border border-gray-200 pl-10 pr-4 py-2 rounded-lg focus:outline-none focus:shadow-outline text-gray-600 font-normal focus:ring-main focus:border-main" name="search" placeholder="Search..." type="text" />
+			<input :value="props.modelValue" @input="$emit('update:modelValue', $event.target.value)" autocomplete="off" class="text-sm w-full border border-gray-200 pl-10 pr-4 py-2 rounded-lg focus:outline-none focus:shadow-outline text-gray-600 font-normal focus:ring-green-500 focus:border-green-500" name="search" placeholder="Search..." type="text" />
 			<div class="absolute top-0 left-0 inline-flex items-center p-2">
 				<svg class="w-6 h-6 text-gray-400" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 					<rect height="24" stroke="none" width="24" x="0" y="0" />
