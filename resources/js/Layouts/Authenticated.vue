26ce0bc2d37776bb30580f8e2e4757e2717a87b1
Mohamed jinas <j@live.mv>
2022-07-05 22:29:36 +0500

    change colors

--- resources/js/Layouts/Authenticated.vue
+++ resources/js/Layouts/Authenticated.vue
@@ -12,13 +12,13 @@ const showingNavigationDropdown = ref(false);
 </script>
 
 <template>
-	<div class="flex overflow-hidden bg-gray-100 h-screen">
+	<div class="flex overflow-hidden bg-white h-screen">
 		<div class="hidden md:flex md:flex-shrink-0">
 			<div class="flex flex-col w-64">
-				<div class="flex flex-col flex-grow pt-5 overflow-y-auto border-r bg-green-700">
+				<div class="flex flex-col flex-grow pt-5 overflow-y-auto border-r bg-gray-100">
 					<div class="flex flex-col items-center flex-shrink-0 px-4">
 						<Link :href="route('admin.dashboard')" class="px-8 text-left focus:outline-none">
-							<h2 class="block p-2 text-xl font-bold tracking-tighter transition duration-500 ease-in-out transform cursor-pointer text-neutral-200 hover:text-neutral-200 uppercase">Sams Management</h2>
+							<h2 class="block p-2 text-xl font-bold tracking-tighter transition duration-500 ease-in-out transform cursor-pointer text-gray-600 hover:text-gray-700 uppercase">Sams Management</h2>
 						</Link>
 						<button class="hidden rounded-lg focus:outline-none focus:shadow-outline">
 							<svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
@@ -28,10 +28,10 @@ const showingNavigationDropdown = ref(false);
 						</button>
 					</div>
 					<div class="flex flex-col flex-grow px-4 mt-5">
-						<nav class="flex-1 space-y-1 bg-green-700">
+						<nav class="flex-1 space-y-1">
 							<ul>
 								<li>
-									<Link :class="route().current('admin.dashboard') ? 'text-neutral-200 bg-green-600' : 'text-neutral-200'" :href="route('admin.dashboard')" class="inline-flex items-center w-full px-4 py-2 mt-1 text-base transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline hover:bg-green-900">
+									<Link :class="route().current('admin.dashboard') ? 'text-green-500 bg-white border-l-2 border-green-400' : 'text-gray-600 hover:text-green-500'" :href="route('admin.dashboard')" class="inline-flex items-center w-full px-4 py-2 mt-1 text-base transition duration-500 ease-in-out transform focus:shadow-outline">
 										<svg class="w-6 h-6" fill="none" viewBox="0 0 24 24">
 											<path d="M4.75 6.75C4.75 5.64543 5.64543 4.75 6.75 4.75H17.25C18.3546 4.75 19.25 5.64543 19.25 6.75V17.25C19.25 18.3546 18.3546 19.25 17.25 19.25H6.75C5.64543 19.25 4.75 18.3546 4.75 17.25V6.75Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" />
 											<path d="M9.75 8.75V19" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" />
@@ -42,7 +42,7 @@ const showingNavigationDropdown = ref(false);
 									</Link>
 								</li>
 								<li>
-									<Link :class="route().current('admin.modules.*') ? 'text-neutral-200 bg-green-900' : 'text-neutral-200'" :href="route('admin.modules.index')" class="inline-flex items-center w-full px-4 py-2 mt-1 text-base transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline hover:bg-green-900">
+									<Link :class="route().current('admin.modules.*') ? 'text-green-500 bg-white border-l-2 border-green-400' : 'text-gray-600 hover:text-green-500'" :href="route('admin.modules.index')" class="inline-flex items-center w-full px-4 py-2 mt-1 text-base transition duration-500 ease-in-out transform focus:shadow-outline">
 										<svg class="w-6 h-6" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 											<path d="M19.25 15.25V5.75C19.25 5.19772 18.8023 4.75 18.25 4.75H6.75C5.64543 4.75 4.75 5.64543 4.75 6.75V16.75" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" />
 											<path d="M19.25 15.25H6.75C5.64543 15.25 4.75 16.1454 4.75 17.25C4.75 18.3546 5.64543 19.25 6.75 19.25H19.25V15.25Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" />
@@ -52,7 +52,7 @@ const showingNavigationDropdown = ref(false);
 									</Link>
 								</li>
 								<li>
-									<Link :class="route().current('admin.teachers.*') ? 'text-neutral-200 bg-green-900' : 'text-neutral-200'" :href="route('admin.teachers.index')" class="inline-flex items-center w-full px-4 py-2 mt-1 text-base transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline hover:bg-green-900">
+									<Link :class="route().current('admin.teachers.*') ? 'text-green-500 bg-white border-l-2 border-green-400' : 'text-gray-600 hover:text-green-500'" :href="route('admin.teachers.index')" class="inline-flex items-center w-full px-4 py-2 mt-1 text-base transition duration-500 ease-in-out transform focus:shadow-outline">
 										<svg class="w-6 h-6" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 											<path d="M4.75 10L12 5.75L19.2501 10L12 14.25L4.75 10Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" />
 											<path d="M12.5 10C12.5 10.2761 12.2761 10.5 12 10.5C11.7239 10.5 11.5 10.2761 11.5 10C11.5 9.72386 11.7239 9.5 12 9.5C12.2761 9.5 12.5 9.72386 12.5 10Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
@@ -63,7 +63,7 @@ const showingNavigationDropdown = ref(false);
 									</Link>
 								</li>
 								<li>
-									<Link :class="route().current('admin.students.*') ? 'text-neutral-200 bg-green-900' : 'text-neutral-200 border-neutral-800 hover:border-neutral-800'" :href="route('admin.students.index')" class="inline-flex items-center w-full px-4 py-2 mt-1 text-base transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline hover:bg-green-900">
+									<Link :class="route().current('admin.students.*') ?'text-green-500 bg-white border-l-2 border-green-400' : 'text-gray-600 hover:text-green-500'" :href="route('admin.students.index')" class="inline-flex items-center w-full px-4 py-2 mt-1 text-base transition duration-500 ease-in-out transform focus:shadow-outline">
 										<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 											<path d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
 										</svg>
@@ -72,7 +72,7 @@ const showingNavigationDropdown = ref(false);
 								</li>
 
 								<li>
-									<Link :class="route().current('admin.enrollments.*') ? 'text-neutral-200 bg-green-900' : 'text-neutral-200 border-neutral-800 hover:border-neutral-800'" :href="route('admin.enrollments.index')" class="inline-flex items-center w-full px-4 py-2 mt-1 text-base transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline hover:bg-green-900">
+									<Link :class="route().current('admin.enrollments.*') ? 'text-green-500 bg-white border-l-2 border-green-400' : 'text-gray-600 hover:text-green-500'" :href="route('admin.enrollments.index')" class="inline-flex items-center w-full px-4 py-2 mt-1 text-base transition duration-500 ease-in-out transform focus:shadow-outline">
 										<svg class="w-6 h-6" fill="none" viewBox="0 0 24 24">
 											<circle cx="12" cy="8" r="3.25" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" />
 											<path d="M12.25 19.25H6.94953C5.77004 19.25 4.88989 18.2103 5.49085 17.1954C6.36247 15.7234 8.23935 14 12.25 14" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" />
@@ -83,7 +83,7 @@ const showingNavigationDropdown = ref(false);
 									</Link>
 								</li>
 								<li>
-									<Link class="inline-flex items-center w-full px-4 py-2 mt-1 text-base transition duration-500 ease-in-out transform rounded-lg text-neutral-200 focus:shadow-outline hover:bg-green-900" href="#">
+									<Link class="inline-flex items-center w-full px-4 py-2 mt-1 text-base transition duration-500 ease-in-out transform rounded-lg text-gray-600 focus:shadow-outline hover:text-green-400" href="#">
 										<svg class="w-6 h-6" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 											<path d="M7.25 4.75V19.25M12.25 4.75H5.75C5.19772 4.75 4.75 5.19771 4.75 5.75V18.25C4.75 18.8023 5.19772 19.25 5.75 19.25H13.25M14.75 16.25V6.75C14.75 5.64543 15.6454 4.75 16.75 4.75H17.25C18.3546 4.75 19.25 5.64543 19.25 6.75V16.25L17 19.25L14.75 16.25Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" />
 										</svg>
@@ -93,24 +93,24 @@ const showingNavigationDropdown = ref(false);
 								</li>
 							</ul>
 
-							<p class="px-4 pt-4 font-medium uppercase text-neutral-200">Actions</p>
+							<p class="px-4 pt-4 font-medium uppercase text-gray-600">Actions</p>
 							<ul>
 								<li>
-									<Link :href="route('logout')" as="button" class="inline-flex items-center w-full px-4 py-2 mt-1 text-base transition duration-500 ease-in-out transform rounded-lg text-neutral-200 focus:shadow-outline" method="post">
+									<Link :href="route('logout')" as="button" class="inline-flex items-center w-full px-4 py-2 mt-1 text-base transition duration-500 ease-in-out transform rounded-lg text-gray-600 focus:shadow-outline" method="post">
 										<span class="ml-4">Logout</span>
 									</Link>
 								</li>
 							</ul>
 						</nav>
 					</div>
-					<div class="flex flex-shrink-0 p-4 px-4 bg-green-900">
+					<div class="flex flex-shrink-0 p-4 px-4 bg-white border-r-4 border-green-500">
 						<a class="flex-shrink-0 block w-full group" href="#">
 							<div class="flex items-center">
 								<div>
 									<img alt class="inline-block rounded-full h-9 w-9" src="https://robohash.org/jinas" />
 								</div>
 								<div class="ml-3">
-									<p class="text-sm font-medium text-neutral-200">{{ $page.props.auth.user.name }}</p>
+									<p class="text-sm font-medium text-gray-600">{{ $page.props.auth.user.name }}</p>
 								</div>
 							</div>
 						</a>
--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -35,36 +35,17 @@ const props = defineProps({
 		</template>
 
 		<div class="py-12">
-            <article class="p-2 rounded-2xl">
-  <a href="" class="flex flex-col justify-end h-full p-6 bg-white sm:p-8 rounded-xl hover:bg-opacity-90">
-    <div class="mt-16">
-      <p class="text-xs font-medium text-gray-500">
-        26/05/2021
-      </p>
-      <h5 class="mt-2 text-xl font-bold text-white">
-        Custom Helper to Improve the Rails phone_to Helper
-      </h5>
-      <div class="flex items-center justify-between mt-6">
-        <p class="text-lg font-medium text-transparent bg-clip-text bg-gradient-to-r from-red-400 to-red-600">
-          Rails
-        </p>
-        <ul class="flex space-x-1">
-          <li class="inline-block rounded-full text-white text-xs font-medium px-3 py-1.5 bg-gray-800">
-            Snippet
-          </li>
-          <li class="inline-block rounded-full text-white text-xs font-medium px-3 py-1.5 bg-gray-800">
-            Info
-          </li>
-          <li class="inline-block rounded-full text-white text-xs font-medium px-3 py-1.5 bg-gray-800">
-            Tip
-          </li>
-        </ul>
-      </div>
-    </div>
-  </a>
-</article>
+			<article class="p-2 rounded-2xl">
+				<a class="flex flex-col justify-end h-full p-6 bg-white sm:p-8 rounded-xl hover:bg-opacity-90" href>
+					<div>
+						<h5 class="mt-2 text-xl font-bold text-gray-800">Welcome, {{ $page.props.auth.user.name }} 👋</h5>
+						<div class="flex items-center justify-between mt-6">
+							<p class="text-lg font-medium text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-green-600">Account Type: {{ $page.props.auth.user.role }}</p>
+						</div>
+					</div>
+				</a>
+			</article>
 			<div class="items-center flex flex-wrap mt-8">
-
 				<div class="px-3 mb-6 w-full md:w-1/2">
 					<div class="bg-white shadow rounded-lg h-150">
 						<div class="px-6 py-4">
@@ -72,7 +53,13 @@ const props = defineProps({
 								<h3 class="mr-3 text-base text-gray-700 font-bold">Total Modules</h3>
 							</div>
 							<p class="flex items-center text-4xl mb-4">{{ props.total_modules }}</p>
-							<p class="flex items-center text-sm mb-4 text-green-600" v-if="props.modules_today > 0">{{ props.modules_today }} new modules today!</p>
+							<div class="inline-flex gap-2 p-1 text-green-600 bg-green-100 rounded" v-if="props.modules_today > 0">
+								<svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+									<path d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
+								</svg>
+
+								<span class="text-xs font-medium">{{ props.modules_today }} new modules today!</span>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -84,8 +71,14 @@ const props = defineProps({
 								<h3 class="mr-3 text-base text-gray-700 font-bold">Total Students</h3>
 							</div>
 							<p class="flex items-center text-4xl mb-4">{{ props.total_students }}</p>
-                            <p class="flex items-center text-sm mb-4 text-green-600" v-if="props.students_today > 0">{{ props.students_today }} new students registered today!</p>
-						</div>
+							<div class="inline-flex gap-2 p-1 text-blue-600 bg-blue-100 rounded" v-if="props.students_today > 0">
+								<svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+									<path d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
+								</svg>
+
+								<span class="text-xs font-medium">{{ props.students_today }} new students registered today!</span>
+							</div>
+                        </div>
 					</div>
 				</div>
 
