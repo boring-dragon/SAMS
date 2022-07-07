71cd60a0f1d65cc7368ff61eb126a1403eb36ee2
Mohamed jinas <j@live.mv>
2022-07-07 09:27:19 +0500

    WIP

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -14,10 +14,11 @@ defineProps({
 	<section>
 		<div class="max-w-5xl px-4 py-32 mx-auto lg:items-center lg:flex">
 			<div class="max-w-3xl mx-auto text-center">
-				<h1 class="text-3xl font-extrabold sm:text-5xl">
-					<strong class="font-extrabold text-green-500 sm:block">SAMS</strong>
-					the only Attendance System you will ever need.
+				<h1 class="text-3xl font-extrabold sm:text-6xl">
+					<strong class="font-extrabold text-green-600 sm:block">SAMS</strong>
+
 				</h1>
+                <p>The only Attendance System you will ever need.</p>
 
 				<div class="flex flex-wrap justify-center gap-4 mt-8">
 					<a class="block w-full px-12 py-3 text-sm font-medium text-white bg-green-600 rounded shadow sm:w-auto active:bg-green-500 hover:bg-green-700 focus:outline-none focus:ring" href="/register">Get Started</a>
