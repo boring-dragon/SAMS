e29fd7e248f701ba87ca343683d44fd6244595a2
Mohamed jinas <j@live.mv>
2022-07-07 09:05:37 +0500

    WIP

---
+++ public/build/assets/Form.af972a62.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./plugin-vue_export-helper.21dcd24c.js";import{o as e,c as o}from"./app.ee471600.js";const c={};function t(a,n){return e(),o("div")}var f=r(c,[["render",t]]);export{f as default};
